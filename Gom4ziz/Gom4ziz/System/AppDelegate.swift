dd103766c2b680eccad00e0ef5479f4a327dc669
HoJong <pjh00098@gmail.com>
2022-10-21 22:29:25 +0900

    Test: Add UserInfoRepository Tests
    파이어베이스를 활용한 유저레포지토리 테스트가 정상적으로 실행되는지 확인하는 테스트를 추가함!
    유저 추가, 유저 요청, 유저 삭제 등의 기능이 정상적으로 동작하는 것을 확인하였다.

--- Gom4ziz/Gom4ziz/System/AppDelegate.swift
+++ Gom4ziz/Gom4ziz/System/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by JongHo Park on 2022/10/19.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -12,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
