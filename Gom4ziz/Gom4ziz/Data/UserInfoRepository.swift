e842b3e22cbc553fc8634d2c90b4e20031af315c
HoJong <pjh00098@gmail.com>
2022-10-21 11:05:05 +0900

    Feat: Add UserInfo Repository & Firebase Implementation
    유저인포를 추가하고, 불러올 수 있는 레포지토리를 추가함!

--- Gom4ziz/Gom4ziz/Data/UserInfoRepository.swift
+++ Gom4ziz/Gom4ziz/Data/UserInfoRepository.swift
@@ -11,5 +11,40 @@ import RxSwift
 
 protocol UserInfoRepository {
     func requestUserInfo(of uid: String) -> Observable<UserInfo>
-    func addUserInfo(userInfo: UserInfo)
+    func addUserInfo(userInfo: UserInfo) -> Single<Void>
+}
+
+final class FirebaseUserInfoRepository {
+    static let shared: UserInfoRepository = FirebaseUserInfoRepository()
+    private let db: Firestore = Firestore.firestore()
+    private let userInfoIdCache: UserInfoIdCache = UserInfoIdCache()
+    private var collectionName: String {
+        ProcessInfo().isRunningTests ? "TestUserInfos": "UserInfos"
+    }
+    private init() { }
+}
+
+extension FirebaseUserInfoRepository: UserInfoRepository {
+
+    func requestUserInfo(of uid: String) -> Observable<UserInfo> {
+        getUserInfoRef(of: uid)
+            .rx
+            .decodable(as: UserInfo.self, source: userInfoIdCache.cached(uid) ? .cache: .server)
+            .do(onNext: { [weak self] in
+                self?.userInfoIdCache.cache($0.id)
+            })
+    }
+
+    func addUserInfo(userInfo: UserInfo) -> Single<Void> {
+        getUserInfoRef(of: userInfo.id)
+            .rx
+            .setData(userInfo)
+    }
+}
+
+// MARK: - Private extension
+private extension FirebaseUserInfoRepository {
+    func getUserInfoRef(of uid: String) -> DocumentReference {
+        db.collection(collectionName).document(uid)
+    }
 }
