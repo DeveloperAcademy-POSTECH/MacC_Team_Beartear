88ee5e0b4c7d5f977cf18d3b69c55037a06878b6
HoJong <pjh00098@gmail.com>
2022-10-23 11:08:15 +0900

    Chore: Add Kakao dependency & secret key
    카카오 의존성 (Rx Kakao share) 을 추가하고, 카카오 관련 API key 파일 참조를 pbxproj 에 추가하였음.
    
    REST API Key 는 이그노어에 추가해서, 따로 파일을 공유하는 형식으로 진행하겠음

--- Gom4ziz/Gom4ziz/System/AppDelegate.swift
+++ Gom4ziz/Gom4ziz/System/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import Firebase
+import RxKakaoSDKCommon
 import UIKit
 
 @main
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        RxKakaoSDK.initSDK(appKey: "eb92b48052cc747b19537d2ed3f9f8a2")
         return true
     }
 
