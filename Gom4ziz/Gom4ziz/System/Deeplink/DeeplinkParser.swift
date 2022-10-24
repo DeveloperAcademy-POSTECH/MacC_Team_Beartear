e6ddd7df74cd709383f6e348db39a34f68a0e94f
HoJong <pjh00098@gmail.com>
2022-10-24 14:06:14 +0900

    Test: Add Deeplink Parser Tests
    초대 딥링크가 정상적으로 파싱되는지, 또 이상한 딥링크 url 일 때 파싱이 정상적으로 실패하는지를 확인하는 테스트를 추가!

--- Gom4ziz/Gom4ziz/System/Deeplink/DeeplinkParser.swift
+++ Gom4ziz/Gom4ziz/System/Deeplink/DeeplinkParser.swift
@@ -8,16 +8,19 @@
 import Foundation
 
 struct DeeplinkParser {
+
     func parseURLToDeeplink(_ link: URL) -> Deeplink? {
         let string = link.absoluteString
-        if string.contains("invite") {
-            let splited = string.components(separatedBy: "?")
-            guard let plansRoomId: String = splited.last?.components(separatedBy: "&").filter({ $0.contains("plansRoomId") }).first?.components(separatedBy: "=").last else {
-                return nil
+        if string.contains("kakaolink") {
+            if string.contains("invite") {
+                let splited = string.components(separatedBy: "?")
+                guard let plansRoomId: String = splited.last?.components(separatedBy: "&").filter({ $0.contains("plansRoomId") }).first?.components(separatedBy: "=").last else {
+                    return nil
+                }
+                return .invitePromiseRoom(plansRoomId: plansRoomId)
             }
-            return .invitePromiseRoom(plansRoomId: plansRoomId)
         }
-
         return nil
     }
+
 }
