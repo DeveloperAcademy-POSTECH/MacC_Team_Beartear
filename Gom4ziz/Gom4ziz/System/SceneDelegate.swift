50743c4c0bd360861d259dbd9747c1b32a00130b
HoJong <pjh00098@gmail.com>
2022-10-24 10:21:20 +0900

    Feat: Add Kakao Message sender
    카카오 메시지 템플릿을 활용, 약속방 초대에 대한 메시지를 공유하는 기능을 추가함.
    또한 Usecase 모델을 정의 -> 프로토콜로 추상화해 사용하는 클라이언트쪽에서 테스트를 용이하게 함

--- Gom4ziz/Gom4ziz/System/SceneDelegate.swift
+++ Gom4ziz/Gom4ziz/System/SceneDelegate.swift
@@ -12,7 +12,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene  else {
+            return
+        }
 
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        // 테스트를 위해서 루트 뷰컨트롤러를 변경할 수 있습니다.
+        changeRootViewController(ShareTestViewController())
+    }
+
+    // TODO: DeepLink 처리 함수. 추후 카카오 딥링크를 파싱해야함
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        print(URLContexts)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -31,5 +43,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
 
+    }
+}
+
+private extension SceneDelegate {
+
+
+    /// 윈도우의 루트 뷰컨을 바꾸는 함수
+    /// - Parameter controller: 바꿀 뷰컨트롤러
+    func changeRootViewController(_ controller: UIViewController) {
+        window?.rootViewController = controller
     }
 }
