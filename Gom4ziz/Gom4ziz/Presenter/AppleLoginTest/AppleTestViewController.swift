a3e7a3734b381558e8e30b90a6861202a092fa0d
SH0123 <sanghyoboy@naver.com>
2022-10-27 00:27:57 +0900

    Feat: Add apple sign in except auto login

--- Gom4ziz/Gom4ziz/Presenter/AppleLoginTest/AppleTestViewController.swift
+++ Gom4ziz/Gom4ziz/Presenter/AppleLoginTest/AppleTestViewController.swift
@@ -10,15 +10,39 @@ import UIKit
 
 final class AppleTestViewController: UIViewController {
 
-    private lazy var appleLoginManager = AppleLoginManager(vc: self)
+    private lazy var appleLoginManager: AppleLoginManager = {
+        let manager = AppleLoginManager(vc: self)
+        return manager
+    }()
     private let appleTestView = AppleTestView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        appleLoginManager.delegate = self
+        bindingView()
     }
 
     override func loadView() {
         self.view = appleTestView
     }
 
+    private func bindingView() {
+        appleTestView.appleLoginButton.addTarget(self, action: #selector(tapAppleLoginButton), for: .touchUpInside)
+    }
+
+    // MARK: - @objc function
+
+    @objc private func tapAppleLoginButton() {
+        appleLoginManager.startSignInWithAppleFlow()
+    }
+}
+
+extension AppleTestViewController: AppleLoginManagerDelegate {
+    func appleLoginSuccess() {
+        print("success")
+    }
+
+    func appleLoginFail() {
+        print("fail")
+    }
 }
