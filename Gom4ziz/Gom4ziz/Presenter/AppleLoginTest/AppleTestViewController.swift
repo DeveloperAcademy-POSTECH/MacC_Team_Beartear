59f250f065a0b482a0e6d37ca82aca697b9d9c95
SH0123 <sanghyoboy@naver.com>
2022-10-26 17:29:05 +0900

    Feat: Add AppleLoginManager

---
+++ Gom4ziz/Gom4ziz/Presenter/AppleLoginTest/AppleTestViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AppleTestViewController.swift
+//  Gom4ziz
+//
+//  Created by sanghyo on 2022/10/26.
+//
+
+import Foundation
+import UIKit
+
+final class AppleTestViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
