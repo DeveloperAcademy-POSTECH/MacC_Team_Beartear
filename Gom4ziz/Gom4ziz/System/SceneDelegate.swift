2460564af2847cadf560ea7e3ccf97077d6dfa1b
SH0123 <sanghyoboy@naver.com>
2022-10-27 00:07:54 +0900

    Chore: Change google info plist

--- Gom4ziz/Gom4ziz/System/SceneDelegate.swift
+++ Gom4ziz/Gom4ziz/System/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
         // 테스트를 위해서 루트 뷰컨트롤러를 변경할 수 있습니다.
-        changeRootViewController(ShareTestViewController())
+        changeRootViewController(AppleTestViewController())
     }
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
