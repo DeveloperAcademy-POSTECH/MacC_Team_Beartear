a4b1f8314ae71479e60db033021ab9615c2bebc8
jungsesang <jeongjaeyn@gmail.com>
2022-11-21 16:22:12 +0900

    Feat: Add FirebaseArtworkReviewRepository

---
+++ Gom4ziz/Source/Data/Repository/FirebaseArtworkReviewRepository.swift
@@ -0,0 +1,38 @@
+//
+//  FirebaseArtworkReviewRepository.swift
+//  Gom4ziz
+//
+//  Created by 정재윤 on 2022/11/21.
+//
+
+import Foundation
+
+import FirebaseFirestore
+import RxSwift
+import RxRelay
+
+protocol ArtworkReviewRepository {
+    func fetchArtworkReview(of artworkId: Int, _ artworkReviewId: String) -> Observable<ArtworkReview>
+}
+
+final class FirebaseArtworkReviewRepository {
+    static let shared: ArtworkReviewRepository = FirebaseArtworkReviewRepository()
+    private let db: Firestore = Firestore.firestore()
+    
+    private init() { }
+}
+
+extension FirebaseArtworkReviewRepository: ArtworkReviewRepository {
+    func fetchArtworkReview(of artworkId: Int, _ artworkReviewId: String) -> Observable<ArtworkReview> {
+        getArtworkReviewRef(of: artworkId, artworkReviewId)
+            .rx
+            .decodable(as: ArtworkReview.self)
+    }
+}
+
+private extension FirebaseArtworkReviewRepository {
+    func getArtworkReviewRef(of artworkId: Int, _ artworkReviewId: String) -> DocumentReference {
+        db.collection(CollectionName.artwork).document("\(artworkId)")
+            .collection(CollectionName.artworkReview).document(artworkReviewId)
+    }
+}
