fa2b6e89a4e7c08616714da3f606c945c7ff934d
SH0123 <sanghyoboy@naver.com>
2022-11-21 17:54:35 +0900

    Feat: Add weekly artwork status datatype

---
+++ Gom4ziz/Source/Presenter/WeeklyArtworkStatus.swift
@@ -0,0 +1,33 @@
+//
+//  WeeklyArtworkStatus.swift
+//  Gom4ziz
+//
+//  Created by sanghyo on 2022/11/21.
+//
+
+import Foundation
+
+enum WeeklyArtworkStatus {
+    case notRequested
+    case loading(last: Artwork?)
+    case loaded(Artwork)
+    case noMoreData
+    case failed(Error)
+}
+
+extension WeeklyArtworkStatus: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .notRequested:
+            return "아직 요청되지 않음"
+        case .loading(let last):
+            return "로딩중임. 이전 데이터 \(String(describing: last))"
+        case .loaded(let data):
+            return "\(data) 로드 완료"
+        case .noMoreData:
+            return "더이상 받을 데이터가 없음. 다음 데이터 받는 날 까지 기다려야함"
+        case .failed(let error):
+            return "\(error) 종류의 에러 발생 "
+        }
+    }
+}
