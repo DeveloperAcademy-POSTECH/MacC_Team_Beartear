ac6ded8ad33a6951194812cb70d821361490374c
HoJong <pjh00098@gmail.com>
2022-11-21 11:07:42 +0900

    Chore: Add Source Folder

---
+++ Gom4ziz/Source/Presenter/QuestionAnswer/QuestionAnswerView.swift
@@ -0,0 +1,37 @@
+//
+//  QuestionAnswerView.swift
+//  Gom4ziz
+//
+//  Created by JongHo Park on 2022/11/21.
+//
+
+import UIKit
+
+final class QuestionAnswerView: BaseAutoLayoutUIView {
+
+}
+
+extension QuestionAnswerView {
+
+    func addSubviews() {
+
+    }
+
+    func setUpConstraints() {
+
+    }
+
+    func setUpUI() {
+
+    }
+
+}
+
+#if DEBUG
+import SwiftUI
+struct QuestionAnswerPreview: PreviewProvider {
+    static var previews: some View {
+        QuestionAnswerView().toPreview()
+    }
+}
+#endif
