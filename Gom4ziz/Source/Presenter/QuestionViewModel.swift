04adf45c90e8796241cd96ec6b6d964c6c32e357
SH0123 <sanghyoboy@naver.com>
2022-11-21 19:56:28 +0900

    Feat: change viewmodel variable to behaviorSubject

--- Gom4ziz/Source/Presenter/QuestionViewModel.swift
+++ Gom4ziz/Source/Presenter/QuestionViewModel.swift
@@ -14,16 +14,21 @@ final class QuestionViewModel {
     private let requestArtworkUsecase: RequestArtworkUsecase
     private let disposeBag: DisposeBag = .init()
     
-    let artwork: PublishSubject<Artwork> = .init()
+    let artwork: BehaviorSubject<WeeklyArtworkStatus> = .init(value: .notRequested)
     
     init(requestArtworkUsecase: RequestArtworkUsecase) {
         self.requestArtworkUsecase = requestArtworkUsecase
     }
     
     func requestArtwork(_ userLastArtworkId: Int) {
+        artwork.onNext(.loading)
         requestArtworkUsecase.requestNextArtwork(userLastArtworkId)
             .subscribe(onNext: { [weak self] in
-                self?.artwork.onNext($0)
+                let loadedStatus = WeeklyArtworkStatus.loaded($0)
+                self?.artwork.onNext(loadedStatus)
+            },
+                       onError: { error in
+                // TODO: error의 종류에 따라 다른 처리, 해당 document가 없다는 에러일 경우 .noMoreData 방출 처리
             })
             .disposed(by: disposeBag)
     }
