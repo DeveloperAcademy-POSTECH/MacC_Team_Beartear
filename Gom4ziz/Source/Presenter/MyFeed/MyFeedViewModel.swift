b30c7ed8abe472ecd84580fa5d77eaa23d3c1ea2
jungsesang <jeongjaeyn@gmail.com>
2022-11-22 17:25:13 +0900

    Rename: Rename Observable

--- Gom4ziz/Source/Presenter/MyFeed/MyFeedViewModel.swift
+++ Gom4ziz/Source/Presenter/MyFeed/MyFeedViewModel.swift
@@ -15,9 +15,9 @@ final class MyFeedViewModel {
     private let fetchArtworkReviewUseCase: FetchArtworkReviewUseCase
     private let fetchArtworkDescriptionUseCase: FetchArtworkDescriptionUseCase
     private let fetchHighlightUseCase: FetchHighlightUseCase
-    let artworkReview: BehaviorRelay<Loadable<ArtworkReview>> = .init(value: .notRequested)
-    let artworkDescription: BehaviorRelay<Loadable<ArtworkDescription>> = .init(value: .notRequested)
-    let highlights: BehaviorRelay<Loadable<[Highlight]>> = .init(value: .notRequested)
+    let artworkReviewObservable: BehaviorRelay<Loadable<ArtworkReview>> = .init(value: .notRequested)
+    let artworkDescriptionObservable: BehaviorRelay<Loadable<ArtworkDescription>> = .init(value: .notRequested)
+    let highlightObservable: BehaviorRelay<Loadable<[Highlight]>> = .init(value: .notRequested)
     private let disposeBag: DisposeBag = .init()
     
     init(fetchArtworkReviewUseCase: FetchArtworkReviewUseCase,
@@ -29,37 +29,37 @@ final class MyFeedViewModel {
     }
     
     func fetchArtworkReview(of artworkId: Int, _ userId: String) {
-        artworkReview.accept(.isLoading(last: nil))
+        artworkReviewObservable.accept(.isLoading(last: nil))
         fetchArtworkReviewUseCase.fetchArtwokReview(of: artworkId, userId)
             .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] in
-                self?.artworkReview.accept(.loaded($0))
+                self?.artworkReviewObservable.accept(.loaded($0))
             }, onError: { [weak self] in
-                self?.artworkReview.accept(.failed($0))
+                self?.artworkReviewObservable.accept(.failed($0))
             })
             .disposed(by: disposeBag)
     }
     
     func fetchArtworkDescription(of artworkId: Int) {
-        artworkDescription.accept(.isLoading(last: nil))
+        artworkDescriptionObservable.accept(.isLoading(last: nil))
         fetchArtworkDescriptionUseCase.fetchArtworkDescription(of: artworkId)
             .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] in
-                self?.artworkDescription.accept(.loaded($0))
+                self?.artworkDescriptionObservable.accept(.loaded($0))
             }, onError: { [weak self] in
-                self?.artworkDescription.accept(.failed($0))
+                self?.artworkDescriptionObservable.accept(.failed($0))
             })
             .disposed(by: disposeBag)
     }
     
     func fetchHighlight(of artworkId: Int, _ userId: String) {
-        highlights.accept(.isLoading(last: nil))
+        highlightObservable.accept(.isLoading(last: nil))
         fetchHighlightUseCase.fetchHighlight(of: artworkId, userId)
             .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] in
-                self?.highlights.accept(.loaded($0))
+                self?.highlightObservable.accept(.loaded($0))
             }, onError: { [weak self] in
-                self?.highlights.accept(.failed($0))
+                self?.highlightObservable.accept(.failed($0))
             })
             .disposed(by: disposeBag)
     }
