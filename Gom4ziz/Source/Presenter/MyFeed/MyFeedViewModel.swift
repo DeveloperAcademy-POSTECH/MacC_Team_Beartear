56388303c543ad68ce5c6344563b96a98ef91089
jungsesang <jeongjaeyn@gmail.com>
2022-11-22 11:20:54 +0900

    Feat: Add fetch artworkDescription domain logic

--- Gom4ziz/Source/Presenter/MyFeed/MyFeedViewModel.swift
+++ Gom4ziz/Source/Presenter/MyFeed/MyFeedViewModel.swift
@@ -11,12 +11,16 @@ import RxCocoa
 import RxSwift
 
 final class MyFeedViewModel {
+    
     private let fetchArtworkReviewUseCase: FetchArtworkReviewUseCase
+    private let fetchArtworkDescriptionUseCase: FetchArtworkDescriptionUseCase
     let artworkReview: BehaviorRelay<Loadable<ArtworkReview>> = .init(value: .notRequested)
+    let artworkDescription: BehaviorRelay<Loadable<ArtworkDescription>> = .init(value: .notRequested)
     private let disposeBag: DisposeBag = .init()
     
-    init(fetchArtworkReviewUseCase: FetchArtworkReviewUseCase) {
+    init(fetchArtworkReviewUseCase: FetchArtworkReviewUseCase, fetchArtworkDescriptionUseCase: FetchArtworkDescriptionUseCase) {
         self.fetchArtworkReviewUseCase = fetchArtworkReviewUseCase
+        self.fetchArtworkDescriptionUseCase = fetchArtworkDescriptionUseCase
     }
     
     func fetchArtworkReview(of artworkId: Int, _ userId: String) {
@@ -30,4 +34,17 @@ final class MyFeedViewModel {
             })
             .disposed(by: disposeBag)
     }
+    
+    func fetchArtworkDescription(of artworkId: Int) {
+        artworkDescription.accept(.isLoading(last: nil))
+        fetchArtworkDescriptionUseCase.fetchArtworkDescription(of: artworkId)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.artworkDescription.accept(.loaded($0))
+            }, onError: { [weak self] in
+                self?.artworkDescription.accept(.failed($0))
+            })
+            .disposed(by: disposeBag)
+    }
+    
 }
