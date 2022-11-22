10bd100929e20cad1f582d2b9473e3fb0d788845
jungsesang <jeongjaeyn@gmail.com>
2022-11-22 15:45:53 +0900

    Test: HighlightRepository Unit Test file (Not working so far)

--- Gom4zizTests/Source/HighlightRepositoryTests.swift
+++ Gom4zizTests/Source/HighlightRepositoryTests.swift
@@ -1,8 +1,8 @@
 //
-//  ArtworkReviewRepositoryTests.swift
+//  HighlightRepositoryTests.swift
 //  Gom4zizTests
 //
-//  Created by JongHo Park on 2022/11/21.
+//  Created by 정재윤 on 2022/11/22.
 //
 
 import Gom4ziz
@@ -11,16 +11,18 @@ import XCTest
 import RxTest
 import RxBlocking
 
-final class ArtworkReviewRepositoryTests: XCTestCase {
-
+final class HighlightRepositoryTests: XCTestCase {
+    
     private var artworkReviewRepository: FirebaseArtworkReviewRepository!
     private var artworkRepository: FirebaseArtworkRepository!
+    private var highlightRepository: FirebaseHighlightRepository!
     private let testArtwork: Artwork = Artwork.mockData
 
     override func setUpWithError() throws {
         try super.setUpWithError()
         artworkRepository = FirebaseArtworkRepository()
         artworkReviewRepository = FirebaseArtworkReviewRepository()
+        highlightRepository = FirebaseHighlightRepository()
         try artworkRepository.addArtwork(artwork: testArtwork).toBlocking().last()
     }
 
@@ -28,6 +30,7 @@ final class ArtworkReviewRepositoryTests: XCTestCase {
         try artworkRepository.deleteArtwork(artwork: testArtwork).toBlocking().last()
         artworkReviewRepository = nil
         artworkRepository = nil
+        highlightRepository = nil
         try super.tearDownWithError()
     }
 
@@ -35,11 +38,14 @@ final class ArtworkReviewRepositoryTests: XCTestCase {
     func test_아트워크리뷰를_정상적으로_등록하는지() throws {
         // given
         let review: ArtworkReview = .mockData
+        let highlights: [Highlight] = Highlight.mockData
         // when
         try artworkReviewRepository.addArtworkReview(of: testArtwork.id, review).toBlocking().last()
         let loadedReview = try artworkReviewRepository.fetchArtworkReview(of: testArtwork.id, review.uid).toBlocking().first()
+        
+        let list = try highlightRepository.fetchHighlight(of: testArtwork.id, review.uid).toBlocking().first()
+        
         // then
-        XCTAssertEqual(loadedReview, review)
+        XCTAssertEqual(list, highlights)
         try artworkReviewRepository.deleteArtworkReview(of: testArtwork.id, review: review).toBlocking().last()
-    }
-}
+    }}
