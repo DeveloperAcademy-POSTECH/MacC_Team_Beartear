dca7ea11270505aa75271ca1307d0f7e31d66978
이가은 <rriver2@naver.com>
2022-11-22 20:35:05 +0900

    feat: Rxswift whereField extendion 추가

--- Gom4ziz/Source/Extension/RxSwift+Firestore.swift
+++ Gom4ziz/Source/Extension/RxSwift+Firestore.swift
@@ -16,14 +16,14 @@ enum RxFirestoreError: Error {
 extension RxFirestoreError: LocalizedError {
     var errorDescription: String? {
         switch self {
-        case .documentIsNotExist:
-            return "데이터가 존재하지 않습니다."
+            case .documentIsNotExist:
+                return "데이터가 존재하지 않습니다."
         }
     }
 }
 
 extension Reactive where Base: DocumentReference {
-
+    
     /// get document snapshot with given document reference
     /// - Parameter source: FirestoreSource
     /// - Returns: Observable of document snapshot
@@ -44,7 +44,7 @@ extension Reactive where Base: DocumentReference {
             return Disposables.create()
         }
     }
-
+    
     /// get decodable type value with given document reference
     /// - Parameters:
     ///   - as: decodable type
@@ -54,7 +54,7 @@ extension Reactive where Base: DocumentReference {
         document(source: source)
             .map { try $0.data(as: T.self) }
     }
-
+    
     /// set encodable data to given document reference
     /// - Parameter data: encodable type
     /// - Returns: Single whether set data is fail or success
@@ -74,7 +74,7 @@ extension Reactive where Base: DocumentReference {
             return Disposables.create()
         }
     }
-
+    
     /// delete document
     /// - Returns: Single whether deletion is fail or success
     func deleteDocument() -> Single<Void> {
@@ -89,7 +89,7 @@ extension Reactive where Base: DocumentReference {
             return Disposables.create()
         }
     }
-
+    
     func updateDocument(updateQuery: [AnyHashable: Any]) -> Single<Void> {
         Single.create { single -> Disposable in
             base.updateData(updateQuery) { error in
@@ -99,7 +99,7 @@ extension Reactive where Base: DocumentReference {
                 }
                 single(.success(()))
             }
-
+            
             return Disposables.create()
         }
     }
@@ -146,6 +146,46 @@ extension Reactive where Base: CollectionReference {
     }
 }
 
+extension Reactive where Base: Query {
+    /// get collection filtered by  isLessThanOrEqualTo snapshot with given collection reference
+    /// - Parameter source: FirestoreSource
+    /// - Returns: Observable of collection snapshot
+    func whereField(source: FirestoreSource = .default) -> Observable<QuerySnapshot> {
+        Observable.create { observer in
+            base.getDocuments(source: source) { snapshot, error in
+                    guard error == nil else {
+                        observer.onError(error!)
+                        return
+                    }
+                    guard let snapshot = snapshot else {
+                        observer.onError(RxFirestoreError.documentIsNotExist)
+                        return
+                    }
+                    observer.onNext(snapshot)
+                    observer.onCompleted()
+                }
+            return Disposables.create()
+        }
+    }
+    
+    /// get decodable type value with given document reference
+    /// - Parameters:
+    ///   - as: decodable type
+    ///   - source: FirestoreSource
+    /// - Returns: Observable of decodable type
+    func decodable<T: Decodable>(as: [T].Type, source: FirestoreSource = .default) -> Observable<[T]> {
+        whereField(source: source)
+            .map { snapshot in
+                var temp: [T] = []
+                for document in snapshot.documents {
+                    let data = try document.data(as: T.self)
+                    temp.append(data)
+                }
+                return temp
+            }
+    }
+}
+
 extension Reactive where Base: Firestore {
     func runTransaction(updateBlock: @escaping (Transaction, NSErrorPointer) -> Any?) -> Single<Void> {
         Single.create { single -> Disposable in
@@ -156,7 +196,7 @@ extension Reactive where Base: Firestore {
                 }
                 single(.success(()))
             }
-
+            
             return Disposables.create()
         }
     }
