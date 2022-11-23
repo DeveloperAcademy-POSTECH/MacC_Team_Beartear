0a1801a6e73a9a41c730579c4f8cc736213ab137
HoJong <pjh00098@gmail.com>
2022-11-23 14:57:09 +0900

    Feat: Add AsyncImage View
    HTTP 요청을 통해 이미지를 비동기적으로 불러오는 AsyncImageView를 추가했습니다!
    
    또한 디스크 캐시를 이용하므로, 한 번 이미지를 불러온 뒤론 디스크에서 이미지를 불러오게 됩니다. 
    만약 이미지를 불러오다가 실패한 경우, 다시 시도할 수도 있습니다.

---
+++ Gom4ziz/Source/Extension/UIImageView.swift
@@ -0,0 +1,32 @@
+//
+//  UIImageView.swift
+//  Gom4ziz
+//
+//  Created by JongHo Park on 2022/11/23.
+//
+
+import UIKit
+
+extension UIImageView {
+
+    func loadImage(url: URL, option: NetworkImageCacheManager.Option = .useCache, completion: ((Error?) -> Void)? = nil) {
+        NetworkImageCacheManager.shared.loadImage(url: url) { image, error in
+            DispatchQueue.main.async {
+                guard error == nil, let image else {
+                    completion?(error)
+                    return
+                }
+                self.image = image
+                completion?(nil)
+            }
+        }
+    }
+
+    func loadImage(url string: String, option: NetworkImageCacheManager.Option = .useCache, completion: ((Error?) -> Void)? = nil) {
+        guard let url = URL(string: string) else {
+            return
+        }
+        loadImage(url: url, option: option, completion: completion)
+    }
+
+}
---
+++ Gom4ziz/Source/Presenter/Component/AsyncImageView.swift
@@ -0,0 +1,95 @@
+//
+//  AsyncImage.swift
+//  Gom4ziz
+//
+//  Created by JongHo Park on 2022/11/23.
+//
+
+import UIKit
+
+final class AsyncImageView: UIImageView {
+
+    private let url: String
+    private var errorIndicator: UIButton?
+    private let progressView: UIView
+
+    init(
+        url string: String,
+        progressView: UIView = UIActivityIndicatorView(style: .large),
+        contentMode: ContentMode = .scaleAspectFill
+    ) {
+        self.url = string
+        self.progressView = progressView
+        super.init(frame: .zero)
+        self.contentMode = contentMode
+        self.isUserInteractionEnabled = true
+        loadImageFromURL()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+// MARK: - 내부구현
+private extension AsyncImageView {
+    func addIndicator() {
+        addSubview(progressView)
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: topAnchor),
+            progressView.leftAnchor.constraint(equalTo: leftAnchor),
+            progressView.rightAnchor.constraint(equalTo: rightAnchor),
+            progressView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+        if let indicator = progressView as? UIActivityIndicatorView {
+            indicator.startAnimating()
+        }
+    }
+
+    func addErrorIndicator(_ error: Error) {
+        errorIndicator = UIButton()
+        var configuration: UIButton.Configuration = .filled()
+        configuration.background.backgroundColor = .red
+        configuration.title = "다시 시도"
+        configuration.image = UIImage(systemName: "wifi.exclamationmark")
+        errorIndicator!.configuration = configuration
+        addSubview(errorIndicator!)
+        errorIndicator!.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            errorIndicator!.centerXAnchor.constraint(equalTo: centerXAnchor),
+            errorIndicator!.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+        errorIndicator!.addTarget(self, action: #selector(loadImageFromURL), for: .touchUpInside)
+    }
+
+    @objc func loadImageFromURL() {
+        startLoading()
+        loadImage(url: url) { [weak self] error in
+            self?.progressView.removeFromSuperview()
+            guard error == nil else {
+                self?.addErrorIndicator(error!)
+                return
+            }
+        }
+    }
+
+    func startLoading() {
+        addIndicator()
+        errorIndicator?.removeFromSuperview()
+        errorIndicator = nil
+    }
+}
+
+#if DEBUG
+import SwiftUI
+struct AsyncImageViewPreview: PreviewProvider {
+    static var previews: some View {
+        Group {
+            AsyncImageView(url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/440px-Image_created_with_a_mobile_phone.png").toPreview()
+            AsyncImageView(url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/440px-Image_created_with_a_mobile_phone.png", contentMode: .scaleAspectFit).toPreview()
+            AsyncImageView(url: "https://wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/440px-Image_created_with_a_mobile_phone.png", contentMode: .scaleAspectFit).toPreview()
+        }
+    }
+}
+#endif
