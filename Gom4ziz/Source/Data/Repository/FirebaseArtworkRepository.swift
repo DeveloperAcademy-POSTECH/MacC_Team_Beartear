ac69c9223bc66b5ddffc4bb43a88264383b7c4f7
이가은 <rriver2@naver.com>
2022-11-23 10:04:22 +0900

    fix: 호종 codeReview 반영

--- Gom4ziz/Source/Data/Repository/FirebaseArtworkRepository.swift
+++ Gom4ziz/Source/Data/Repository/FirebaseArtworkRepository.swift
@@ -12,7 +12,7 @@ import RxSwift
 
 protocol ArtworkRepository {
     func requestArtwork(of artworkId: Int) -> Observable<Artwork>
-    func fetchReviewedArtworkList(of artworkId: Int) -> Observable<[Artwork]>
+    func fetchReviewedArtworkList(before artworkId: Int) -> Observable<[Artwork]>
 }
 
 final class FirebaseArtworkRepository {
@@ -28,7 +28,7 @@ extension FirebaseArtworkRepository: ArtworkRepository {
             .decodable(as: Artwork.self)
     }
 
-    func fetchReviewedArtworkList(of artworkId: Int) -> RxSwift.Observable<[Artwork]> {
+    func fetchReviewedArtworkList(before artworkId: Int) -> RxSwift.Observable<[Artwork]> {
         getReviewedArtworkListRef(of: artworkId)
             .rx
             .decodable(as: [Artwork].self)
