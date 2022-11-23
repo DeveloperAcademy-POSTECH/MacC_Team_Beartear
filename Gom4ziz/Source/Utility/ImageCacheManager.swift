5647e9a5f4eeba9f9b6afcc47dbac9b24144965a
HoJong <pjh00098@gmail.com>
2022-11-23 14:57:09 +0900

    Feat: Add AsyncImage View
    HTTP 요청을 통해 이미지를 비동기적으로 불러오는 AsyncImageView를 추가했습니다!
    
    또한 디스크 캐시를 이용하므로, 한 번 이미지를 불러온 뒤론 디스크에서 이미지를 불러오게 됩니다. 
    만약 이미지를 불러오다가 실패한 경우, 다시 시도할 수도 있습니다.

---
+++ Gom4ziz/Source/Utility/ImageCacheManager.swift
@@ -0,0 +1,102 @@
+//
+//  ImageCacheManager.swift
+//  Gom4ziz
+//
+//  Created by JongHo Park on 2022/11/23.
+//
+
+import UIKit
+
+public final class NetworkImageCacheManager {
+
+    enum Option: Equatable {
+        case useCache
+        case onlyRemote
+
+        static func == (lhs: Option, rhs: Option) -> Bool {
+            switch (lhs, rhs) {
+            case (.useCache, .useCache): return true
+            case (.onlyRemote, .onlyRemote): return true
+            default: return false
+            }
+        }
+    }
+
+    public static let shared: NetworkImageCacheManager = NetworkImageCacheManager()
+    private let session: URLSession = URLSession(configuration: .ephemeral)
+    private init() { }
+
+    func loadImage(url: URL, option: Option = .useCache, completion: @escaping (UIImage?, Error?) -> Void) {
+        // 디스크 캐시에 image 데이터가 있으면, 바로 함수를 종료한다.
+
+        if option == .useCache, let image = loadFromDisk(url: url.absoluteString) {
+            completion(image, nil)
+            return
+        }
+
+        let request: URLRequest = URLRequest(url: url, timeoutInterval: 5)
+        session.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                completion(nil, error!)
+                return
+            }
+            guard let data, let response = response as? HTTPURLResponse, 200..<300 ~= response.statusCode else {
+                completion(nil, URLError(.badServerResponse))
+                return
+            }
+
+            guard let image = UIImage(data: data) else {
+                completion(nil, URLError(.badServerResponse))
+                return
+            }
+            // url에서 데이터를 반환했으면, 디스크 캐시에 저장한 후 종료
+            self.saveToDisk(url: url.absoluteString, data: data)
+            completion(image, nil)
+        }.resume()
+    }
+
+    func loadImage(url string: String, option: Option = .useCache, completion: @escaping (UIImage?, Error?) -> Void) {
+        guard let url: URL = URL(string: string) else {
+            completion(nil, URLError(.badURL))
+            return
+        }
+        loadImage(url: url, option: option, completion: completion)
+    }
+}
+
+private extension NetworkImageCacheManager {
+
+    func loadFromDisk(url string: String) -> UIImage? {
+        guard let cachePath = FileManager.cachePath else {
+            return nil
+        }
+        let imagePath: URL = cachePath.appendingPathComponent(string.withoutPunctuations, isDirectory: false)
+
+        guard let data = try? Data(contentsOf: imagePath), let image = UIImage(data: data) else {
+            return nil
+        }
+        return image
+    }
+
+    func saveToDisk(url string: String, data: Data) {
+        guard let cachePath = FileManager.cachePath else {
+            return
+        }
+        let imagePath: URL = cachePath.appendingPathComponent(string.withoutPunctuations, isDirectory: false)
+
+        try? data.write(to: imagePath, options: .atomic)
+    }
+
+}
+
+extension FileManager {
+    static var cachePath: URL? {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
+    }
+}
+
+extension String {
+    var withoutPunctuations: String {
+        return self.components(separatedBy: CharacterSet.punctuationCharacters).joined(separator: "")
+      }
+}
