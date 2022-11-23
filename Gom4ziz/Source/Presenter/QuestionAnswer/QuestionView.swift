b16a49dc7cbe3f83769ddd48ec47f6053b5406c6
HoJong <pjh00098@gmail.com>
2022-11-23 23:00:39 +0900

    Feat: Add Question View

---
+++ Gom4ziz/Source/Presenter/QuestionAnswer/QuestionView.swift
@@ -0,0 +1,142 @@
+//
+//  QuestionView.swift
+//  Gom4ziz
+//
+//  Created by JongHo Park on 2022/11/23.
+//
+
+import UIKit
+
+final class QuestionView: BaseAutoLayoutUIView {
+
+    private let artwork: Artwork
+    private let questionNumberLabel: UILabel = UILabel()
+    private let questionImageView: AsyncImageView
+    private let questionLabel: UILabel = UILabel()
+    private let whiteDivider: UIView = UIView()
+    private let infoContainer: UIView = UIView()
+
+    init(artwork: Artwork) {
+        self.artwork = artwork
+        questionImageView = AsyncImageView(
+            url: artwork.imageUrl,
+            contentMode: .scaleAspectFill,
+            exposure: 0.3,
+            contrast: 0.95)
+        super.init(frame: .zero)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
+
+extension QuestionView {
+
+    func addSubviews() {
+        addSubview(questionImageView)
+        addSubview(infoContainer)
+        infoContainer.addSubviews(questionNumberLabel, questionLabel, whiteDivider)
+    }
+
+    func setUpConstraints() {
+        setUpInfoContainerConstaints()
+        setUpQuestionLabelConstraints()
+        setUpWhiteDividerConstraints()
+        setUpQuestionNumberLabelConstraints()
+        setUpQuestionImageViewConstraints()
+    }
+
+    func setUpUI() {
+        setUpQuestionImageView()
+        setUpQuestionLabel()
+        setUpWhiteDivider()
+        setUpQuestionNumberLabel()
+        setUpSelf()
+    }
+
+}
+
+// MARK: - 오토레이아웃 제약조건 설정
+private extension QuestionView {
+
+    func setUpInfoContainerConstaints() {
+        infoContainer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            infoContainer.topAnchor.constraint(equalTo: topAnchor),
+            infoContainer.leftAnchor.constraint(equalTo: leftAnchor),
+            infoContainer.rightAnchor.constraint(equalTo: rightAnchor),
+            infoContainer.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    func setUpQuestionNumberLabelConstraints() {
+        questionNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            questionNumberLabel.centerXAnchor.constraint(equalTo: infoContainer.centerXAnchor),
+            questionNumberLabel.bottomAnchor.constraint(equalTo: whiteDivider.topAnchor, constant: -5),
+            questionNumberLabel.topAnchor.constraint(equalTo: infoContainer.topAnchor, constant: 48)
+        ])
+
+    }
+    func setUpQuestionLabelConstraints() {
+        questionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            questionLabel.leftAnchor.constraint(equalTo: infoContainer.leftAnchor, constant: 16),
+            questionLabel.rightAnchor.constraint(equalTo: infoContainer.rightAnchor, constant: -16),
+            questionLabel.bottomAnchor.constraint(equalTo: infoContainer.bottomAnchor, constant: -48)
+        ])
+    }
+    func setUpQuestionImageViewConstraints() {
+        questionImageView.translatesAutoresizingMaskIntoConstraints = false
+        questionImageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        NSLayoutConstraint.activate([
+            questionImageView.topAnchor.constraint(equalTo: infoContainer.topAnchor),
+            questionImageView.bottomAnchor.constraint(equalTo: infoContainer.bottomAnchor),
+            questionImageView.leftAnchor.constraint(equalTo: infoContainer.leftAnchor),
+            questionImageView.rightAnchor.constraint(equalTo: infoContainer.rightAnchor),
+        ])
+    }
+    func setUpWhiteDividerConstraints() {
+        whiteDivider.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            whiteDivider.bottomAnchor.constraint(equalTo: questionLabel.topAnchor, constant: -9),
+            whiteDivider.widthAnchor.constraint(equalToConstant: 40),
+            whiteDivider.centerXAnchor.constraint(equalTo: infoContainer.centerXAnchor),
+            whiteDivider.heightAnchor.constraint(equalToConstant: 2)
+        ])
+    }
+}
+
+// MARK: - UI 설정
+private extension QuestionView {
+
+    func setUpSelf() {
+        layer.cornerRadius = 12
+        layer.masksToBounds = true
+    }
+
+    func setUpQuestionNumberLabel() {
+        questionNumberLabel.text = "\(artwork.id)번째 티라미술"
+        questionNumberLabel.font = .systemFont(ofSize: 14, weight: .bold)
+        questionNumberLabel.textColor = .white
+    }
+
+    func setUpQuestionLabel() {
+        questionLabel.textAlignment = .center
+        questionLabel.text = "\(artwork.question)"
+        questionLabel.textColor = .white
+        questionLabel.numberOfLines = 0
+        questionLabel.lineBreakMode = .byWordWrapping
+        questionLabel.font = .systemFont(ofSize: 22, weight: .bold)
+    }
+
+    func setUpQuestionImageView() {
+        
+    }
+
+    func setUpWhiteDivider() {
+        whiteDivider.backgroundColor = .white
+    }
+}
