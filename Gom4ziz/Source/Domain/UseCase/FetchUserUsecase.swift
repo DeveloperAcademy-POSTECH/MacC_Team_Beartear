30298c7a79cd6ec3c4f4661214b3d5beb2a9ba2b
이가은 <rriver2@naver.com>
2022-11-23 19:06:33 +0900

    feat: SceneDelegate에서 User 정보를 DB에서 불러오는 Viewmodel 구현

---
+++ Gom4ziz/Source/Domain/UseCase/FetchUserUsecase.swift
@@ -0,0 +1,37 @@
+//
+//  FetchUserUsecase.swift
+//  Gom4ziz
+//
+//  Created by 이가은 on 2022/11/23.
+//
+
+import Foundation
+import UIKit
+
+import RxSwift
+
+protocol FetchUserUsecase {
+    func fetchUser() -> Observable<User>
+}
+
+struct RealFetchUserUsecase: FetchUserUsecase {
+    private let userRepository: UserRepository
+    
+    init(userRepository: UserRepository = FirebaseUserRepository.shared) {
+        self.userRepository = userRepository
+    }
+    
+    func fetchUser() -> Observable<User> {
+        userRepository.fetchUser(for: getDeviceUUID())
+    }
+}
+
+extension RealFetchUserUsecase {
+    private func getDeviceUUID() -> String {
+#if DEBUG
+        return "mock"
+#else
+        return UIDevice.current.identifierForVendor!.uuidString
+#endif
+    }
+}
