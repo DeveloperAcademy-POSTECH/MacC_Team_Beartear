9f730bed92f873a2c2e9c5ac59c23303b62555e7
SH0123 <sanghyoboy@naver.com>
2022-11-25 10:41:10 +0900

    Feat: Add MainViewController & MainView

---
+++ Gom4ziz/Source/Presenter/Main/MainView.swift
@@ -0,0 +1,28 @@
+//
+//  MainView.swift
+//  Gom4ziz
+//
+//  Created by sanghyo on 2022/11/25.
+//
+
+import UIKit
+
+final class MainView: BaseAutoLayoutUIView {
+    
+}
+
+extension MainView {
+    
+    func addSubviews() {
+        
+    }
+    
+    func setUpConstraints() {
+        
+    }
+    
+    func setUpUI() {
+        
+    }
+    
+}
