2b54d533dc8fc7021a7147c527d1a76533565880
jungsesang <jeongjaeyn@gmail.com>
2022-11-27 14:08:07 +0900

    Feat: Add ErrorViewMessage namespace

---
+++ Gom4ziz/Source/Constants/ErrorViewMessage.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorViewMessage.swift
+//  Gom4ziz
+//
+//  Created by 정재윤 on 2022/11/27.
+//
+
+import Foundation
+
+enum ErrorViewMessage: String {
+    case artwork = "작품을 불러오는데 실패했습니다."
+    case tiramisul = "티라미술을 불러오는데 실패했습니다."
+}
