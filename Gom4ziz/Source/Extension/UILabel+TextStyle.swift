e5391b8f27000d729ccb83d2f1fa1159b5762ce5
HoJong <pjh00098@gmail.com>
2022-11-27 11:32:57 +0900

    Refactor: Fix UILabel and UITextView extension
    같은 타이포라도, 행간이 다른 Label들이 있다.
    또한 attributedText를 사용할 경우, 기존 textAlignment 속성은 먹지 않는다. 따라서 AttributedString전용 alignment를 설정할 필요가 있다.
    따라서 커닝, 라인스페이싱, 행간, 텍스트 정렬 등의 파라미터를 추가로 받을 수 있도록 API 를 수정했습니다

--- Gom4ziz/Source/Extension/UILabel+TextStyle.swift
+++ Gom4ziz/Source/Extension/UILabel+TextStyle.swift
@@ -8,36 +8,46 @@
 import UIKit
 
 extension UILabel {
-    func textStyle(_ typography: Typohgraphy, _ color: UIColor) {
+    func textStyle(
+    _ typography: Typohgraphy,
+    kernValue: Double = 0,
+    lineSpacing: CGFloat = 0,
+    lineHeightMultiple: CGFloat? = nil,
+    alignment: NSTextAlignment = .left,
+    _ color: UIColor) {
         let (fontSize, lineheight, fontWeight) = typography.toValue
         self.font = .systemFont(ofSize: fontSize, weight: fontWeight)
-        self.setLineSpacing(lineSpacing: lineheight)
+        let lineHeight: CGFloat = lineHeightMultiple ?? lineheight
+        setLineSpacing(kernValue: kernValue, lineSpacing: lineSpacing, lineHeightMultiple: lineHeight, alignment: alignment)
         self.textColor = color
     }
     
-    func setLineSpacing(kernValue: Double = 0.0,
-                          lineSpacing: CGFloat = 0.0,
-                          lineHeightMultiple: CGFloat = 0.0) {
-          guard let labelText = self.text else { return }
-          
-          let paragraphStyle = NSMutableParagraphStyle()
-          paragraphStyle.lineSpacing = lineSpacing
-          paragraphStyle.lineHeightMultiple = lineHeightMultiple
-          
-          let attributedString: NSMutableAttributedString
-          if let labelAttributedText = self.attributedText {
-              attributedString = NSMutableAttributedString(attributedString: labelAttributedText)
-          } else {
-              attributedString = NSMutableAttributedString(string: labelText)
-          }
-          
-          attributedString.addAttribute(NSAttributedString.Key.paragraphStyle,
-                                        value: paragraphStyle,
-                                        range: NSMakeRange(0, attributedString.length))
-          attributedString.addAttribute(NSAttributedString.Key.kern,
-                                        value: kernValue,
-                                        range: NSMakeRange(0, attributedString.length))
-          
-          self.attributedText = attributedString
-      }
+    func setLineSpacing(
+        kernValue: Double = 0.0,
+        lineSpacing: CGFloat = 0.0,
+        lineHeightMultiple: CGFloat = 0.0,
+        alignment: NSTextAlignment = .left) {
+            guard let labelText = self.text else { return }
+
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = lineSpacing
+            paragraphStyle.lineHeightMultiple = lineHeightMultiple
+            paragraphStyle.alignment = alignment
+
+            let attributedString: NSMutableAttributedString
+            if let labelAttributedText = self.attributedText {
+                attributedString = NSMutableAttributedString(attributedString: labelAttributedText)
+            } else {
+                attributedString = NSMutableAttributedString(string: labelText)
+            }
+
+            attributedString.addAttribute(NSAttributedString.Key.paragraphStyle,
+                                          value: paragraphStyle,
+                                          range: NSRange(location: 0, length: attributedString.length))
+            attributedString.addAttribute(NSAttributedString.Key.kern,
+                                          value: kernValue,
+                                          range: NSRange(location: 0, length: attributedString.length))
+
+            self.attributedText = attributedString
+        }
 }
--- Gom4ziz/Source/Extension/UITextView+TextStyle.swift
+++ Gom4ziz/Source/Extension/UITextView+TextStyle.swift
@@ -9,23 +9,33 @@ import UIKit
 
 extension UITextView {
     
-    func textStyle(_ typography: Typohgraphy, _ color: UIColor) {
+    func textStyle(
+        _ typography: Typohgraphy,
+        _ color: UIColor
+    ) {
         let (fontSize, lineheight, fontWeight) = typography.toValue
         self.font = .systemFont(ofSize: fontSize, weight: fontWeight)
-        self.setTypingAttributes(lineSpacing: lineheight)
+        self.setTypingAttributes(lineHeightMultiple: lineheight)
         self.textColor = color
     }
     
-    func setTypingAttributes(kernValue: Double = 0.0,
-                        lineSpacing: CGFloat = 0.0,
-                        lineHeightMultiple: CGFloat = 0.0) {
+    func setTypingAttributes(
+        kernValue: Double = 0.0,
+        lineSpacing: CGFloat = 0.0,
+        lineHeightMultiple: CGFloat = 0.0) {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = lineSpacing
         paragraphStyle.lineHeightMultiple = lineHeightMultiple
-        
         let attributes: [NSAttributedString.Key: Any] = [.kern: -0.6,
                                                          .paragraphStyle: paragraphStyle]
         self.typingAttributes = attributes
+
+        guard attributedText.length > 0 else {
+            return
+        }
+        let willSet: NSMutableAttributedString = .init(attributedString: attributedText)
+        willSet.addAttributes(attributes, range: NSRange(location: 0, length: willSet.length - 1))
+        self.attributedText = willSet
     }
     
 }
