592092a054d0a86098fe3a2dcf42ecba45fb8ef8
jungsesang <jeongjaeyn@gmail.com>
2022-11-27 14:08:27 +0900

    Feat: Add ErrorView in Shared Component

---
+++ Gom4ziz/Source/Presenter/Component/ErrorView.swift
@@ -0,0 +1,119 @@
+//
+//  ErrorView.swift
+//  Gom4ziz
+//
+//  Created by 정재윤 on 2022/11/27.
+//
+
+import UIKit
+
+final class ErrorView: BaseAutoLayoutUIView {
+    
+    enum ButtonSize: CGFloat {
+        case width = 154
+        case height = 48
+    }
+    
+    private let message: ErrorViewMessage
+    private let isShowLogo: Bool
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        // TODO: 이미지 불러오는 로직 추가
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var errorMessageLabel: UILabel = {
+        let label = UILabel()
+        label.text = message.rawValue
+        label.textStyle(.Title, .blackFont)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "아래 버튼을 탭해서 다시 불러와 보세요."
+        label.textStyle(.NavigationButton, .systemGray) // TODO: systemGray -> gray3
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var messageStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [errorMessageLabel,
+                                                       captionLabel])
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var retryButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("탭하여 다시 시도 ↻", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 12)
+        button.backgroundColor = .gray4
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var errorStackView: UIStackView = {
+        let stackView = UIStackView()
+        if isShowLogo {
+            stackView.addArrangedSubview(logoImageView)
+        }
+        stackView.addArrangedSubview(messageStackView)
+        stackView.addArrangedSubview(retryButton)
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    init(message: ErrorViewMessage, isShowLogo: Bool = true) {
+        self.message = message
+        self.isShowLogo = isShowLogo
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension ErrorView {
+    
+    func addSubviews() {
+        self.addSubview(errorStackView)
+    }
+    
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            errorStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            errorStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            
+            retryButton.widthAnchor.constraint(equalToConstant: ButtonSize.width.rawValue),
+            retryButton.heightAnchor.constraint(equalToConstant: ButtonSize.height.rawValue)
+        ])
+    }
+    
+    func setUpUI() {
+        
+    }
+    
+}
+
+#if DEBUG
+import SwiftUI
+struct ErrorViewPreview: PreviewProvider {
+    static var previews: some View {
+        ErrorView(message: .tiramisul, isShowLogo: false).toPreview()
+    }
+}
+#endif
