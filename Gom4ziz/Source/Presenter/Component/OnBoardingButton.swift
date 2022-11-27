006bf87b67d5d8ac5adad6d96c93ec467b0392b6
SH0123 <sanghyoboy@naver.com>
2022-11-27 18:37:38 +0900

    Feat: set onBoardingButton & pageViewController

--- Gom4ziz/Source/Presenter/Component/OnBoardingButton.swift
+++ Gom4ziz/Source/Presenter/Component/OnBoardingButton.swift
@@ -9,12 +9,30 @@ import UIKit
 
 final class OnBoardingButton: UIButton {
     
-    override init(frame: CGRect) {
+    init(text: String) {
         super.init(frame: .zero)
+        setUpUI(text: text)
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+}
+
+private extension OnBoardingButton {
+    
+    func setUpUI(text: String) {
+        var configuration = UIButton.Configuration.filled()
+        let font: UIFont = .systemFont(ofSize: 17, weight: .bold)
+        let attributes: AttributeContainer = .init([
+            .font: font,
+            .foregroundColor: UIColor.white
+        ])
+        configuration.attributedTitle = AttributedString(text, attributes: attributes)
+        configuration.background.backgroundColor = .gray4
+        configuration.baseForegroundColor = .white
+        configuration.background.cornerRadius = 0
+        self.configuration = configuration
     }
 }
 
@@ -23,11 +41,10 @@ import SwiftUI
 struct OnBoardingButtonPreview: PreviewProvider {
     static var previews: some View {
         VStack {
-            OnBoardingButton()
+            OnBoardingButton(text: "다음")
                 .toPreview()
-                .frame(width: 100, height: 30)
+                .frame(width: UIScreen.main.bounds.width, height: 50)
         }
     }
 }
 #endif
-
--- Gom4ziz/Source/Presenter/OnBoarding/OnBoardingPageViewController.swift
+++ Gom4ziz/Source/Presenter/OnBoarding/OnBoardingPageViewController.swift
@@ -9,13 +9,36 @@ import UIKit
 
 final class OnBoardingPageViewController: UIPageViewController {
     
+    private let pageViewControllerList: [UIViewController]
+    
+    init(viewControllerList: [UIViewController]) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        pageViewControllerList = viewControllerList
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initPageViewController()
+    }
+}
+
+extension OnBoardingPageViewController {
+    
+    func initPageViewController() {
+        guard let firstPage = pageViewControllerList.first else { return }
+        setViewControllers([firstPage], direction: .forward, animated: true)
+    }
 }
 
 #if DEBUG
 import SwiftUI
 struct OnBoardingPageViewControllerPreview: PreviewProvider {
     static var previews: some View {
-        UINavigationController(rootViewController: OnBoardingPageViewController())
+        UINavigationController(rootViewController: OnBoardingPageViewController(viewControllerList: [OnBoardingFirstViewController()]))
             .toPreview()
     }
 }
