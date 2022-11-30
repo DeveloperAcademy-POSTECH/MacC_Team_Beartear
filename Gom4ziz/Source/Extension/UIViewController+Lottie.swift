a305f964c71094d1bd7c69b24372e88c9fa49d37
HoJong <pjh00098@gmail.com>
2022-11-30 15:20:18 +0900

    Feat: Add tiramisul lottie view and extension to show loading view

---
+++ Gom4ziz/Source/Extension/UIViewController+Lottie.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewController+Lottie.swift
+//  Gom4ziz
+//
+//  Created by JongHo Park on 2022/11/30.
+//
+
+import UIKit
+
+import Lottie
+
+extension UIViewController {
+
+    func showLottieLoadingView() {
+        guard let window = view.window else { return }
+        let lottieLoadingView: LottieAnimationView = LottieAnimationView(name: "tiramisul_lottie")
+        lottieLoadingView.loopMode = .loop
+        lottieLoadingView.backgroundColor = .black.withAlphaComponent(0.5)
+        lottieLoadingView.tag = 1004
+        lottieLoadingView.frame = window.bounds
+        window.addSubview(lottieLoadingView)
+        lottieLoadingView.play()
+    }
+
+    func hideLottieLoadingView() {
+        guard let window = view.window else { return }
+        window.viewWithTag(1004)?.removeFromSuperview()
+    }
+
+}
