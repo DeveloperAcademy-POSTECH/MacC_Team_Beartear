0acb988249ee78f1a0e8b15eee86e4bfad131bc8
HoJong <pjh00098@gmail.com>
2022-12-02 11:49:21 +0900

    Feat: Add Image Source protocol

---
+++ Gom4ziz/Source/Utility/ImageSource.swift
@@ -0,0 +1,20 @@
+//
+//  ImageSource.swift
+//  Gom4ziz
+//
+//  Created by JongHo Park on 2022/12/02.
+//
+
+import UIKit
+
+import RxSwift
+
+/// 특정 url의 이미지를 반환해주는 Image Source
+protocol ImageSource {
+    func fetchImage(of url: URL) -> Single<UIImage>
+}
+
+/// 특정 url의 이미지를 캐싱할 수 있는 Image Source
+protocol CacheableImageSource: ImageSource {
+    func saveToCache(_ image: UIImage)
+}
