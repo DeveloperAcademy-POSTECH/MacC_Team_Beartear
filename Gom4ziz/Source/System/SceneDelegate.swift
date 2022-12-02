e81c9d7ce65a7c05e69499d46352fbd3844f7322
SH0123 <sanghyoboy@naver.com>
2022-12-02 23:04:37 +0900

    Fix: notRequested, loading case 합침

--- Gom4ziz/Source/System/SceneDelegate.swift
+++ Gom4ziz/Source/System/SceneDelegate.swift
@@ -68,7 +68,7 @@ extension SceneDelegate {
                     case .loaded(let user):
                         self.userViewModel.user = user
                             self.changeRootViewController(MainViewController(reviewedArtworkListViewModel: ReviewedArtworkListViewModel(), userViewModel: UserViewModel.shared))
-                    case .isLoading:
+                    case .isLoading, .notRequested:
                         self.changeRootViewController(UserLoadingViewController())
                     case .failed(let error):
                         if let error = error as? UserRequestError, error == .notRegisteredUser {
@@ -76,8 +76,6 @@ extension SceneDelegate {
                         } else {
                             // error 화면
                         }
-                    case .notRequested:
-                        self.changeRootViewController(UserLoadingViewController())
                     }
                 })
             .disposed(by: disposeBag)
