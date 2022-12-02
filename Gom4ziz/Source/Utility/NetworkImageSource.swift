4c7777393d1e8634f3aac52015e671face5302c7
HoJong <pjh00098@gmail.com>
2022-12-02 11:54:05 +0900

    Feat: Add NetworkImageSource

---
+++ Gom4ziz/Source/Utility/NetworkImageSource.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkImageSource.swift
+//  Gom4ziz
+//
+//  Created by JongHo Park on 2022/12/02.
+//
+
+import UIKit
+
+import RxSwift
+
+struct NetworkImageSource: ImageSource {
+
+    private let session: URLSession = URLSession(configuration: .ephemeral)
+
+    func fetchImage(of url: URL) -> Single<UIImage> {
+        let request: URLRequest = URLRequest(url: url, timeoutInterval: 4)
+        return session
+            .rx
+            .data(request: request)
+            .map {
+                guard let image = UIImage(data: $0) else {
+                    throw URLError(.badServerResponse)
+                }
+                return image
+            }
+            .asSingle()
+    }
+
+}
