98869af6a3746a6de94cc4532b8dc0749aa4e790
이가은 <rriver2@naver.com>
2022-12-09 05:40:35 +0900

    feat: ArtworkDescription 개행 추가: 2번째 방법

--- Gom4ziz/Source/Domain/Model/ArtworkDescription.swift
+++ Gom4ziz/Source/Domain/Model/ArtworkDescription.swift
@@ -10,6 +10,18 @@ import Foundation
 struct ArtworkDescription: Codable, Identifiable {
     let id: Int
     let content: String
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        let tempContent = try container.decode(String.self, forKey: .content)
+        self.content = tempContent.convertNewLine
+    }
+    
+    init(id: Int, content: String) {
+        self.id = id
+        self.content = content
+    }
 }
 
 extension ArtworkDescription: CustomStringConvertible {
