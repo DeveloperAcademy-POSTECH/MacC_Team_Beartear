ce8ee2309efb365d0723be36cf313283aa409e50
HoJong <pjh00098@gmail.com>
2022-12-08 12:45:35 +0900

    Refactor: Fix QuestionAnswerView textView
    TextView가 입력한 텍스트에 따라 길어지도록 수정하였음

--- Gom4ziz/Source/Presenter/QuestionAnswer/QuestionAnswerPage/QuestionAnswerView.swift
+++ Gom4ziz/Source/Presenter/QuestionAnswer/QuestionAnswerPage/QuestionAnswerView.swift
@@ -16,6 +16,8 @@ final class QuestionAnswerView: BaseAutoLayoutUIView {
     private let questionView: QuestionView
     private let myThinkLabel: SectionTitleView = SectionTitleView(title: "나의 생각")
     private let disposeBag: DisposeBag = .init()
+    private let scrollView: UIScrollView = UIScrollView()
+    private let baseInsets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 16, right: -16)
     let answerInputTextView: PlaceholderTextView = .init(placeholder: "내용을 입력하세요")
 
     init(artwork: Artwork) {
@@ -23,6 +25,7 @@ final class QuestionAnswerView: BaseAutoLayoutUIView {
         self.questionView = QuestionView(artwork: artwork)
         super.init(frame: .zero)
         self.backgroundColor = .white
+        self.setUpKeyboardObserver()
     }
 
     required init?(coder: NSCoder) {
@@ -35,38 +38,86 @@ final class QuestionAnswerView: BaseAutoLayoutUIView {
 extension QuestionAnswerView {
 
     func addSubviews() {
-        addSubview(questionView)
-        addSubview(myThinkLabel)
-        addSubview(answerInputTextView)
+        addSubview(scrollView)
+        scrollView.addSubviews(
+            questionView,
+            myThinkLabel,
+            answerInputTextView
+        )
     }
 
     func setUpConstraints() {
+
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            scrollView.leftAnchor.constraint(equalTo: leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: rightAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            scrollView.contentLayoutGuide.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32)
+        ])
+        scrollView.contentInset = baseInsets
+
         questionView.translatesAutoresizingMaskIntoConstraints = false
         questionView.setContentHuggingPriority(.defaultHigh, for: .vertical)
         NSLayoutConstraint.activate([
-            questionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 12),
-            questionView.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 16),
-            questionView.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor, constant: -16)
+            questionView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: 12),
+            questionView.leftAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leftAnchor),
+            questionView.rightAnchor.constraint(equalTo: scrollView.contentLayoutGuide.rightAnchor)
         ])
 
         myThinkLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            myThinkLabel.leftAnchor.constraint(equalTo: questionView.leftAnchor),
+            myThinkLabel.leftAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leftAnchor),
             myThinkLabel.topAnchor.constraint(equalTo: questionView.bottomAnchor, constant: 20),
-            myThinkLabel.rightAnchor.constraint(equalTo: questionView.rightAnchor)
+            myThinkLabel.rightAnchor.constraint(equalTo: scrollView.contentLayoutGuide.rightAnchor)
         ])
 
         answerInputTextView.translatesAutoresizingMaskIntoConstraints = false
+        answerInputTextView.isScrollEnabled = false
         NSLayoutConstraint.activate([
-            answerInputTextView.leftAnchor.constraint(equalTo: questionView.leftAnchor),
-            answerInputTextView.rightAnchor.constraint(equalTo: questionView.rightAnchor),
+            answerInputTextView.leftAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leftAnchor),
+            answerInputTextView.rightAnchor.constraint(equalTo: scrollView.contentLayoutGuide.rightAnchor),
             answerInputTextView.topAnchor.constraint(equalTo: myThinkLabel.bottomAnchor, constant: 20),
-            answerInputTextView.bottomAnchor.constraint(equalTo: keyboardLayoutGuide.topAnchor, constant: -16)
+            answerInputTextView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            answerInputTextView.heightAnchor.constraint(greaterThanOrEqualToConstant: 200)
         ])
     }
 
     func setUpUI() {
         addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(sender:))))
+    }
+
+}
+
+// MARK: - 키보드가 올라올 시 스크롤뷰의 콘텐츠 오프셋을 조정하는 부분입니다.
+private extension QuestionAnswerView {
+
+    func setUpKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillAppear(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+                let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+                    return
+            }
+            let contentInset = UIEdgeInsets(
+                top: 0.0,
+                left: 16,
+                bottom: keyboardFrame.size.height + 16,
+                right: -16)
+            scrollView.contentInset = contentInset
+        scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.size.height, right: 0)
+        scrollView.scrollToBottom()
+    }
+
+    @objc func keyboardWillHide() {
+        let contentInset = baseInsets
+        scrollView.contentInset = contentInset
+        scrollView.scrollIndicatorInsets = .zero
     }
 
 }
