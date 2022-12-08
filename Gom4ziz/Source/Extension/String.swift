b56bebd9204459c6a5f035b5ca46ce88816a7a3f
이가은 <rriver2@naver.com>
2022-12-09 05:22:27 +0900

    feat: ArtworkDescription 개행 추가

---
+++ Gom4ziz/Source/Extension/String.swift
@@ -0,0 +1,14 @@
+//
+//  String.swift
+//  Gom4ziz
+//
+//  Created by 이가은 on 2022/12/09.
+//
+
+import Foundation
+
+extension String {
+    var convertNewLine: String {
+        return self.replacingOccurrences(of: "\\n", with: "\n")
+      }
+}
